67a896694ac0fd235bfed87263edf7e5520911dc
Carlo E. Prelz <ghub@fluido.as>
2013-11-26 11:48:32 +0100

    oops

--- mrblib/mrf89_transmit.rb
+++ mrblib/mrf89_transmit.rb
@@ -2,8 +2,8 @@
 
 class Mrf89
   def prepare_for_transmit
-    s.set_para(1,0x8c) # packet mode
-    s.set_para(0xd,0x8) # FTXRXIREG
-    s.set_para(0xe,0x1d) # FTPRIREG
+    set_para(1,0x8c) # packet mode
+    set_para(0xd,0x8) # FTXRXIREG
+    set_para(0xe,0x1d) # FTPRIREG
   end
 end
