20db50041d459ddacb70afa5e9b51b84c1b8ca6a
Carlo E. Prelz <ghub@fluido.as>
2013-12-16 16:26:14 +0100

    cont

--- test/fb.rb
+++ test/fb.rb
@@ -6,8 +6,13 @@ loggo("Ueppolo: #{sx}x#{sy}")
 touch=nil
 loop do
   vs=[rand(sx),rand(sy),rand(sx),rand(sy),rand(0xffffff)]
+
+  if(rand(5)==0)
+    f.rect(*vs)
+  else
+    f.line(*vs)
+  end
   
-  f.rect(*vs)
   nt,px,py=f.status
   unless(nt==touch)
     touch=nt
