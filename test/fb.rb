91dc323aab4791e22e6574ab4903bf351cc616a6
Carlo E. Prelz <ghub@fluido.as>
2013-12-16 16:01:49 +0100

    cont

--- test/fb.rb
+++ test/fb.rb
@@ -2,13 +2,13 @@
 
 f=Fb::new('/dev/fb0','/dev/input/by-path/platform-sun4i-ts-event')
 sx,sy=f.size
-
+loggo("Ueppolo: #{sx}x#{sy}")
 touch=nil
 loop do
+  f.line(rand(sx),rand(sy),rand(sx),rand(sy),rand(0xffffff))
   nt,px,py=f.status
   unless(nt==touch)
     touch=nt
     loggo("#{touch ? 'CLICK' : 'release'} at #{px},#{py}")
   end
-  f.line(rand(sx),rand(sy),rand(sx),rand(sy),rand(0xffffff))
 end
