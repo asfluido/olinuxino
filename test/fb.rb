8dd8b3b1d436fa6257154a5e8c7cb4df5f11d30e
Carlo E. Prelz <ghub@fluido.as>
2013-12-16 14:36:03 +0100

    cont

--- test/fb.rb
+++ test/fb.rb
@@ -1,6 +1,12 @@
 #!/usr/src/mruby/bin/mruby
 
 f=Fb::new('/dev/fb0','/dev/input/by-path/platform-sun4i-ts-event')
-p f
 
-msleep(10000)
+touch=false
+loop do
+  nt,px,py=f.status
+  if(nt!=touch)
+    touch=nt
+    loggo("#{touch ? 'CLICK' : 'release'} at #{px},#{py}")
+  end
+end
