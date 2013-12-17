4a6accdd4168d7c8cd38c307dd469972ddf321c2
Carlo E. Prelz <ghub@fluido.as>
2013-12-17 11:37:27 +0100

    cont

--- src/oxino.c
+++ src/oxino.c
@@ -117,7 +117,7 @@ void mrb_olinuxino_gem_init(mrb_state* mrb)
   mrb_define_method(mrb,c,"line",mrb_fb_line,MRB_ARGS_REQ(5));
   mrb_define_method(mrb,c,"rect",mrb_fb_rect,MRB_ARGS_REQ(5));
   mrb_define_method(mrb,c,"save_calibdata",mrb_fb_save_calibdata,MRB_ARGS_REQ(4));
-  mrb_define_method(mrb,c,"calibdata",mrb_fb_save_calibdata,MRB_ARGS_NONE());
+  mrb_define_method(mrb,c,"calibdata",mrb_fb_calibdata,MRB_ARGS_NONE());
 }
 
 void mrb_olinuxino_gem_final(mrb_state* mrb)
